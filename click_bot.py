1cbcf9d10425727a562b28dc293c743af72c2ff9
we722 <shaminroy681@gmail.com>
2025-05-02 11:58:01 +0530

    Add files via upload
    Update

--- click_bot.py
+++ click_bot.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request
 import json
 import os
@@ -16,4 +15,5 @@ def webhook():
     return '', 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
